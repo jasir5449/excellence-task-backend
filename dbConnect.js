892dbd04825b4ec29448902e996451f9e6f3e0e2
Jasir Parakkattil <jasir5449@gmail.com>
2023-09-21 15:53:35 +0400

    mongodb url added in env file

--- dbConnect.js
+++ dbConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://jasir5449:woPJ12rhzwaVM8XZ@cluster0.4b5wc.mongodb.net/excellence-task' , {useNewUrlParser : true , useUnifiedTopology : true})
+mongoose.connect(process.env.MONGO_URL , {useNewUrlParser : true , useUnifiedTopology : true})
 
 const connection = mongoose.connection
 
